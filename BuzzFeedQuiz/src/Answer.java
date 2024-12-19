712042a68526c560604f65b6f90ae9759824f852
anna-s-kim <anna@Annas-MacBook-Air-2.local>
2024-12-19 09:47:38 -0500

    BuzzFeed Quiz

---
+++ BuzzFeedQuiz/src/Answer.java
@@ -0,0 +1,18 @@
+/*
+ * Irene Feng 10/12/2022
+ * This is an answer with a Category it corresponds to
+ */
+// Anna Kim and Emanuella Odell Dec 2024
+//We did everything together
+
+public class Answer {
+
+    String label;
+    Category cat;
+
+    // Constructor
+    Answer(String label, Category c) {
+        this.label = label;
+        this.cat = c;
+    }
+}
